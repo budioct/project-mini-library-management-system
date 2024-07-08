185c9c2d802a407fcabf542e8dd03cb0045e3cc0
Budhi Octaviansyah <budioct@gmail.com>
2024-07-08 08:44:42 +0700

    start project

---
+++ src/main/java/anak/om/mamat/latihan/LatihanApplication.java
@@ -0,0 +1,13 @@
+package anak.om.mamat.latihan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LatihanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/anak/om/mamat/latihan/LatihanApplicationTests.java
@@ -0,0 +1,13 @@
+package anak.om.mamat.latihan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LatihanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
