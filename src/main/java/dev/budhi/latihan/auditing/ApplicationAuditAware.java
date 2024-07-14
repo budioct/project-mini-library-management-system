0759edd86f3369e02e7626c788be2654dfe05d66
Budhi Octaviansyah <budioct@gmail.com>
2024-07-14 11:29:58 +0700

    add auditing

---
+++ src/main/java/dev/budhi/latihan/auditing/ApplicationAuditAware.java
@@ -0,0 +1,31 @@
+package dev.budhi.latihan.auditing;
+
+import dev.budhi.latihan.modules.users.UserEntity;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware implements AuditorAware<Long> {
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        Authentication authentication =
+                SecurityContextHolder
+                        .getContext()
+                        .getAuthentication();
+        if (authentication == null ||
+                !authentication.isAuthenticated() ||
+                authentication instanceof AnonymousAuthenticationToken
+        ) {
+            return Optional.empty();
+        }
+
+        UserEntity userPrincipal = (UserEntity) authentication.getPrincipal();
+
+        return Optional.ofNullable(userPrincipal.getId());
+    }
+
+}
