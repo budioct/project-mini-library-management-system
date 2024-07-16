e2dea1e05ba57ddcfc015d7c2865311ed37cd3a3
Budhi Octaviansyah <budioct@gmail.com>
2024-07-16 21:38:51 +0700

    add feature Role and Permission endpoint, note: authorize level method

--- src/main/java/dev/budhi/latihan/rest/AdminRest.java
+++ src/main/java/dev/budhi/latihan/rest/AdminRest.java
@@ -1,28 +1,36 @@
 package dev.budhi.latihan.rest;
 
 import io.swagger.v3.oas.annotations.Hidden;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admin")
+@PreAuthorize("hasRole('ADMIN')") // hasRole(ADMIN.name())
 public class AdminRest {
 
+    // note: authorize level method
+
     @GetMapping
+    @PreAuthorize("hasAuthority('admin:read')")
     public String get() {
         return "GET:: admin controller";
     }
     @PostMapping
     @Hidden
+    @PreAuthorize("hasAuthority('admin:create')")
     public String post() {
         return "POST:: admin controller";
     }
     @PutMapping
     @Hidden
+    @PreAuthorize("hasAuthority('admin:update')")
     public String put() {
         return "PUT:: admin controller";
     }
     @DeleteMapping
     @Hidden
+    @PreAuthorize("hasAuthority('admin:delete')")
     public String delete() {
         return "DELETE:: admin controller";
     }
